d8c39a7ac1ae8d4bef2ace9cba001692f23a1ec7
mrx <alex.vilkov@bk.ru>
2021-01-19 23:33:37 +0300

    ...

--- cgi-bin/my.py
+++
@@ -1,5 +0,0 @@
-#!/user/bin/env python3
-
-print("Content-type: text/html")
-print()
-print("<H1>Hello world!</H1>")
---
+++ my.py
@@ -0,0 +1,9 @@
+#!/user/bin/env python3
+
+print("Content-type: text/html")
+print()
+print("<H1>Hello world!</H1>")
+
+# print('Content-Type: text/plain')
+# print('')
+# print('<H1>Hello world!</H1>')
