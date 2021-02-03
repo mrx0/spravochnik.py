ce3e5274761751e39e0d370f7401bc56947e5c40
iNval_iD <alx.vilkov@gmail.com>
2021-02-03 18:01:52 +0800

    index.html
    по умолчанию на главной странице теперь всегда загружается весь список оборудования

--- js/functions.js
+++ js/functions.js
@@ -21,13 +21,13 @@ $("body").on("click", ".k-button", function(){
             // Показываем менюшку с кнопками по типам
             $("#main_window_header").show();
 
-            //Запросим данные по типам оборудования
+            // Запросим данные по типам оборудования
             getDataFromDB('items_types');
 
-            //Запросим данные по всему оборудованию
+            // Запросим данные по всему оборудованию
             getItmesFromDB(0)
 
-            //Запросим данные
+            // Запросим данные
             //getDataFromDB ($(this).attr("id"));
         }
         // - workers_button
@@ -48,27 +48,27 @@ $("body").on("click", ".k-button", function(){
         }
     }
 
-    //Если кнопка типа оборудования
+    // Если кнопка типа оборудования
     if ($(this).attr("data-status") == 'item_type_button') {
 
-        //Удаляем у всех кнопок из main_menu_button класс k-state-active, чтобы они не выглядели выделенными
+        // Удаляем у всех кнопок из main_menu_button класс k-state-active, чтобы они не выглядели выделенными
         $('[data-status="item_type_button"]').removeClass('k-state-active k-state-focused');
         // $('[data-status="item_type_button"]').removeClass('k-state-focused');
         // Добавляем нажатой кнопке из main_menu_button класс k-state-active, чтобы она выглядела выделенной
         $(this).addClass('k-state-active k-state-focused');
         // $(this).addClass('k-state-focused');
 
-        //Запросим данные по всему оборудованию
+        // Запросим данные по всему оборудованию
         getItmesFromDB($(this).attr("data-value"));
     }
 });
 
-//Функция получения данных ... !!! дописать каких
+// Функция получения данных по типам оборудования / сотрудникам
 function getDataFromDB (flag) {
     // Куда передаём данные по умолчанию и откуда будем ждать ответ (скрипт *.py)
     let link = "py/get_types.py";
 
-    //Если мы хотим получить значения по-другому флагу
+    // Если мы хотим получить значения по-другому флагу
     // - данные по сотруднику
     if (flag == 'workers_button'){
         link = "py/get_workers.py";
@@ -98,7 +98,7 @@ function getDataFromDB (flag) {
         // Действие, при ответе с сервера
         success: function (res) {
             // Выводим в консоль
-            //console.log(res);
+            // console.log(res);
             // console.log(res.data);
 
             //Если результат success
@@ -113,9 +113,9 @@ function getDataFromDB (flag) {
                     $('#main_window_header_buttons').append('<span title="Все" data-status="item_type_button" data-value="0" role="button" class="k-button k-state-active k-state-focused">Все</span>');
 
                     res.data.forEach(function(element) {
-                        //console.log(element['name']);
+                        // console.log(element['name']);
 
-                        //Каждый отдельный элемент в отдельную кнопку
+                        // Каждый отдельный элемент в отдельную кнопку
                         $('#main_window_header_buttons').append('<span id="item_type_'+element['id']+'" data-status="item_type_button" data-value="'+element['id']+'" role="button" class="k-button" title="'+element['name']+'">'+element['name']+'</span>');
                     });
                 }
