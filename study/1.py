d652565d9e0209c893df48d2a10ba9e8eb79e966
lexx_avp <lexxavp@gmail.com>
2021-02-06 17:22:31 +0300

    #2  пользовательский ввод и условные конструкции

---
+++ study/1.py
@@ -0,0 +1,7 @@
+age = int (input ("Сколько тебе лет?"))
+if (age >= 25):
+    print (" можно одному ")
+elif (age >= 18) and (age < 25):
+    print ("Тебе можно только с мамой")
+else:
+    print (" FALL ")
