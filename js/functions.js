ecb2a8d36a6dced96bb8c298f6ddc0465381f91d
iNval_iD <alx.vilkov@gmail.com>
2021-02-15 14:40:28 +0800

    Контекстное меню
    тест

--- js/functions.js
+++ js/functions.js
@@ -357,3 +357,24 @@ function moveWorkerOrStaffInStaff (worker_id, staff_id, target_staff_id){
     })
 
 }
+
+//Для теста контекстного меню
+// let menu = document.querySelector('.menu');
+// function showMenu(x, y){
+// menu.style.left = x + 'px';
+// menu.style.top = y + 'px';
+// menu.classList.add('show-menu');
+// }
+// function hideMenu(){
+// menu.classList.remove('show-menu');
+// }
+// function onContextMenu(e){
+// e.preventDefault();
+// showMenu(e.pageX, e.pageY);
+// document.addEventListener('mousedown', onMouseDown, false);
+// }
+// function onMouseDown(e){
+// hideMenu();
+// document.removeEventListener('mousedown', onMouseDown);
+// }
+// document.addEventListener('contextmenu', onContextMenu, false);
